b9b35aad95d4af65ce97f3c1c9b220f4f67b2830
Lucielee-mendes <lucielee28@gmail.com>
2023-09-10 03:08:56 -0300

    splash02

---
+++ app/src/main/java/com/example/fundatecheroes/splash/view/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.example.fundatecheroes.splash.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.fundatecheroes.R
+import com.example.fundatecheroes.login.view.LoginActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splashTimeOut)
+
+    }
+}
