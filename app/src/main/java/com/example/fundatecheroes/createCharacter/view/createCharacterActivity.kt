2a56f947dc63198e6a41936eeeab639892d49a63
Lucielee-mendes <lucielee28@gmail.com>
2023-10-08 17:59:07 -0300

    criarPersonagem

---
+++ app/src/main/java/com/example/fundatecheroes/createCharacter/view/createCharacterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.fundatecheroes.createCharacter.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fundatecheroes.R
+
+class createCharacterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_character)
+    }
+}
